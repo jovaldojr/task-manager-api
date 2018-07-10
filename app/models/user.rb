9e2959b09102833d1d0d432f676202760e3bae1f
Jovaldo Junior <jovaldojr@gmail.com>
2018-07-10 15:49:48 +0000

    Updating User#info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  	devise :database_authenticatable, :registerable,
+    	   :recoverable, :rememberable, :trackable, :validatable
 
+    validates_uniqueness_of :auth_token
+
+    def info
+    	"#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,16 @@ RSpec.describe User, type: :model do
 	it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 	it { is_expected.to validate_confirmation_of(:password) }
 	it { is_expected.to allow_value('jovaldojr@gmail.com').for(:email) }
+	it { is_expected.to validate_uniqueness_of(:auth_token) }
 
+
+	describe '#info' do
+		it 'returns email, created_at and a Token' do
+			user.save! 
+
+			allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+
+			expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
+		end
+	end
 end
