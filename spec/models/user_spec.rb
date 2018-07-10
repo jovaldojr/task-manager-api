1f0f5824f7d36ef9b4d19756cf8c9f6a9186a07f
Jovaldo Junior <jovaldojr@gmail.com>
2018-07-10 13:01:59 +0000

    Adding action create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe User, type: :model do
 	let(:user){ build(:user) }
 
 	it { is_expected.to validate_presence_of(:email) }
-	it { is_expected.to validate_uniqueness_of(:email).case_insensitiverails g  }
+	it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 	it { is_expected.to validate_confirmation_of(:password) }
-	it { is_expected.to allow_value("jovaldojr@gmail.com").for(:email) }
+	it { is_expected.to allow_value('jovaldojr@gmail.com').for(:email) }
 
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,32 +4,67 @@ RSpec.describe 'Users API', type: :request do
 	let!(:user) { create(:user) }
 	let(:user_id) { user.id }
 
-	before { host! "api.taskmanager.test" }
+	before { host! 'api.taskmanager.test' }
 
-	describe "GET /users/:id" do
+	describe 'GET /users/:id' do
 		before do
-			headers = { "Accept" => "application/vnd.taskmanager.v1" }
+			headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
 			get "/users/#{user_id}", params: {}, headers: headers
 		end
 
-		context "when the user exists" do
-			it "returns the user" do
+		context 'when the user exists' do
+			it 'returns the user' do
 				user_response = JSON.parse(response.body)
-				expect(user_response["id"]).to eq(user_id)
+				expect(user_response['id']).to eq(user_id)
 			end
 			# staus Ok
-			it "returns status code 200" do
+			it 'returns status code 200' do
 				expect(response).to have_http_status(200)
 			end
 		end
 
 		# staus Usuario Não existe
-		context "when the user does not exists" do
+		context 'when the user does not exists' do
 			let(:user_id) { 100000 } # Definindo novo usuário para testar o usuário
-			it "returns status code 404" do
+			it 'returns status code 404' do
 				expect(response).to have_http_status(404)
 			end
 		end
 	end
 
+	describe 'POST /users' do
+		
+		before do
+			headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+			post '/users', params: { user: user_params } , headers: headers
+		end
+
+		context	'when the request params are valid' do
+			let(:user_params) { attributes_for(:user) }
+
+			it 'returns status code 201' do
+				expect(response).to have_http_status(201)
+
+			end
+
+			it 'returns json data for the created user' do
+				user_response = JSON.parse(response.body)
+				expect(user_response['email']).to eq(user_params[:email])
+			end
+
+		end
+
+		context	'when the request params are invalid' do
+			let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+			it 'returns status code 422' do
+				expect(response).to have_http_status(422)
+			end
+
+			it 'returns the jason data for the erros' do
+				user_response = JSON.parse(response.body)
+				expect(user_response).to have_key('errors')
+			end
+		end
+	end
 end
